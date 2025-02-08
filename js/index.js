911efd591fe4724b0ea57fc42eecf60015cde402
ManGuevara <mguevaradelacruz@gmail.com>
2025-02-08 16:58:57 -0500

    fixing some warnings and errors on html ans js

--- js/index.js
+++ js/index.js
@@ -18,3 +18,49 @@ menuIcon.addEventListener("click", (e)=>{
     e.preventDefault()
     menu.classList.toggle("nav--show") //shows/hide the menu when click
 })
+
+
+//LOCAL STORAGE
+
+let nVisits = localStorage.getItem("counter")
+
+if (nVisits === null){
+    nVisits = 0;
+}
+else{
+    nVisits = parseInt(nVisits)
+}
+
+nVisits++
+
+localStorage.setItem("counter", nVisits);
+obj("nVisits").textContent = nVisits
+
+// API
+
+async function loadPlace(){
+    const url = 'https://tourist-attraction.p.rapidapi.com/search';
+    const options = {
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': '612cde2624msh0a19a40f231a0b3p1c40e1jsn7bf300f4ec13',
+            'x-rapidapi-host': 'tourist-attraction.p.rapidapi.com',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            location_id: '45963',
+            language: 'en_US',
+            currency: 'USD',
+            offset: '0'
+        })
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result.results);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
